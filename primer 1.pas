33fef1fe7a9c2256385d26b175f1bd7b4a76efdc
Chkhfgjx <116419857+Chkhfgjx@users.noreply.github.com>
2023-02-20 12:58:16 +0300

    Add files via upload

---
+++ primer 1.pas
@@ -0,0 +1,63 @@
+﻿const MAXSIZE = 50;
+type Stack = record  { стек рассчитан на 50 символов }
+      tags: array[1..MAXSIZE] of char;
+      size: integer; { число элементов }
+     end;
+     
+procedure Push( var S: Stack; x: char);
+begin
+  if S.size = MAXSIZE then Exit;  // выход, если произошло переполнение стека
+  S.size := S.size + 1;
+  S.tags[S.size] := x; // добавляем элемент
+end;
+
+function Pop ( var S:Stack ): char;
+begin
+  if S.size = 0 then begin
+    Result := char(255); // 255 - пустой символ, ошибка - стек пуст
+    Exit;
+  end;
+  Result := S.tags[S.size];
+  S.size := S.size - 1;
+end;
+
+function isEmptyStack ( S: Stack ): Boolean;
+begin
+  Result := (S.size = 0);
+end;
+
+  var  expr: string;
+    br1, br2: char;  { угловые скобки }
+    i, k: integer;
+    upper: char;    { верхний символ, снятый со стека }
+    error: Boolean; { признак ошибки }
+    S: Stack;
+    
+begin
+  br1 := '<'; br2 := '>';
+  S.size := 0;
+  error := False;
+  writeln('Введите html-текст');
+  readln(expr);
+  
+  
+  for i:=1 to length(expr) { проходимся по всем символам строки expr }
+ do begin
+       if expr[i] = br1 then begin { открывающая скобка < }
+        Push(S, expr[i]); { втолкнуть в стек }
+        break;
+      end;
+      if expr[i] = br2  then begin { закрывающая скобка < }
+        upper := Pop(S); { снять символ со стека }
+        error := upper <> br1; { ошибка: стек пуст или не та скобка }
+        break;
+      end;
+ 
+    if error then break; // была ошибка, значит, прерываем цикл
+  end; { здесь вставляется цикл с обработкой строки } 
+  
+  
+  if not error  and  isEmptyStack(S) then
+       writeln('Текст верный.')
+  else writeln('Текст составлен неверно.')
+end.
---
+++ primer 2.pas
@@ -0,0 +1,33 @@
+﻿
+begin
+  var file_dlya_zabora_dannykh:text;
+  var file_dlya_vyvoda_dannykh:text;
+  assign(file_dlya_zabora_dannykh, 'D:\Учеба\МДК 05.02  Разработка кода информационных систем\Лабы\ЛР 18\filein_for_2.txt');
+  assign(file_dlya_vyvoda_dannykh, 'D:\Учеба\МДК 05.02  Разработка кода информационных систем\Лабы\ЛР 18\fileout_for_2.txt');
+  
+  var Dannye:array of string;
+  
+  Reset(file_dlya_zabora_dannykh);
+  var kolichestvo_strok:=0;
+  while not eof(file_dlya_zabora_dannykh) do begin
+    kolichestvo_strok += 1;
+    readln(file_dlya_zabora_dannykh);
+  end;
+  
+  SetLength(Dannye, kolichestvo_strok + 1);
+  
+  for var i:=1 to kolichestvo_strok do begin
+    while not eoln(file_dlya_zabora_dannykh) do begin
+      Read(file_dlya_zabora_dannykh, Dannye[i]);
+    end;  
+  end;  
+  
+  close(file_dlya_zabora_dannykh);
+  Rewrite(file_dlya_vyvoda_dannykh);
+  
+  for var i:=kolichestvo_strok downto 1 do begin
+    writeln(file_dlya_vyvoda_dannykh, Dannye[i]);
+  end;
+  
+  close(file_dlya_vyvoda_dannykh);
+end.
